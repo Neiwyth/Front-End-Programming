3b18ee9912d7c407883db9cb58b6f514b53e2e28
Neiwyth <user.example@mailing.com>
2023-10-24 17:24:16 +0300

    MUI assignment, Tabs part

--- todolist-MUI/src/App.jsx
+++ todolist-MUI/src/App.jsx
@@ -1,11 +1,28 @@
+import { Fragment, useState } from 'react'
 import './App.css'
-import TodoList from './components/todolist'
+import { Tab, Tabs } from '@mui/material';
+import TodoList from './components/todolist';
+import Home from './components/home';
 
 function App() {
 
+  const [value, setValue] = useState('Home');
+
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
 
   return (
-    <TodoList />
+    <Fragment>
+      <div className='app-container'>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab value='Home' label='Home' />
+          <Tab value='List' label='Todolist' />
+        </Tabs>
+      </div>
+      {value === 'Home' && <div className='content-container'><Home /></div>}
+      {value === 'List' && <div><TodoList /></div>}
+    </Fragment>
   )
 }
 
